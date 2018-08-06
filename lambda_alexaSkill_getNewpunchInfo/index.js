5823ebde62d219f9b46ef46e33a10fce4de26017
ganonscrub <mboharsik@gmail.com>
2018-08-06 00:22:44 -0400

    Add support for Polidicks headlines

--- lambda_alexaSkill_getNewpunchInfo/index.js
+++ lambda_alexaSkill_getNewpunchInfo/index.js
@@ -11,6 +11,11 @@ const HELP_MESSAGE = 'You can ask me for the top three, five, or ten headlines f
 const HELP_REPROMPT = 'Which would you like to hear?';
 const STOP_MESSAGE = 'Goodbye!';
 
+const SENSATIONALIST_STRING = 'sensationalist';
+const POLIDICKS_STRING = 'polidicks';
+
+const skillBuilder = alexa.SkillBuilders.custom();
+
 const ordinalStrings = [
   'First',
   'Second',
@@ -31,37 +36,39 @@ const GetSensationalistHeadlinesHandler = {
       || (request.type === 'IntentRequest'
         && request.intent.name == 'GetSensationalistHeadlines');
   },
-  async handle(handlerInput){
-    let slots = handlerInput.requestEnvelope.request.intent.slots;
+  async handle(handlerInput){    
+    let count = getHeadlineCount(handlerInput);
     
-    let count = DEFAULT_HEADLINE_COUNT;
-    
-    if (slots.count){
-      if (slots.count.value){
-        count = parseInt(slots.count.value, 10);
-        
-        if (count < DEFAULT_HEADLINE_COUNT)
-          count = DEFAULT_HEADLINE_COUNT;
-        else if (count === 4)
-          count = 5;
-        else if (count > 5)
-          count = MAX_HEADLINE_COUNT;
-      }
-    }
-    
-    let threads = await getHeadlines('sensationalist', count, 'lastPostTime');
-    
-    let headlinesSpokenString = '';
-    let headlinesCardString = '';
-    for (let [idx, thread] of Object.entries(threads)){
-      let ordinal = ordinalStrings[idx];
-      headlinesSpokenString += ` ${ordinal} headline: ${thread.headline}.`;
-      headlinesCardString += `${parseInt(idx, 10) + 1}. ${thread.headline}\n`;
-    }
+    let threads = await getHeadlines(SENSATIONALIST_STRING, count, 'lastPostTime');    
+    let headlineData = getHeadlineData(threads);
     
     let response = handlerInput.responseBuilder
-      .speak(`Here are the latest ${threads.length} news headlines from Facepunch.${headlinesSpokenString}`)
-      .withSimpleCard(SKILL_NAME, headlinesCardString)
+      .speak(`Here are the latest ${threads.length} sensationalist headlines from Facepunch.${headlineData.spokenString}`)
+      .withSimpleCard('Sensationalist Headlines', headlineData.cardString)
+      .getResponse();
+      
+    console.info(`Generate Alexa response:\n${JSON.stringify(response, null, '\t')}`);
+    
+    return response;
+  },
+};
+
+const GetPolidicksHeadlinesHandler = {
+  canHandle(handlerInput){
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name == 'GetPolidicksHeadlines');
+  },
+  async handle(handlerInput){    
+    let count = getHeadlineCount(handlerInput);
+    
+    let threads = await getHeadlines(POLIDICKS_STRING, count, 'lastPostTime');    
+    let headlineData = getHeadlineData(threads);
+    
+    let response = handlerInput.responseBuilder
+      .speak(`Here are the latest ${threads.length} political headlines from Facepunch.${headlineData.spokenString}`)
+      .withSimpleCard('Polidicks Headlines', headlineData.cardString)
       .getResponse();
       
     console.info(`Generate Alexa response:\n${JSON.stringify(response, null, '\t')}`);
@@ -111,17 +118,6 @@ const ErrorHandler = {
   },
 };
 
-const skillBuilder = alexa.SkillBuilders.custom();
-
-exports.handler = skillBuilder
-  .addRequestHandlers(
-    GetSensationalistHeadlinesHandler,
-    HelpHandler,
-    ExitHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
-
 async function getHeadlines(type, count, sortKey){
   let lambdaRequestPayload = {
     type: type,
@@ -146,3 +142,49 @@ async function getHeadlines(type, count, sortKey){
     return [];
   }
 }
+
+function getHeadlineCount(handlerInput){
+  let slots = handlerInput.requestEnvelope.request.intent.slots;
+    
+  let count = DEFAULT_HEADLINE_COUNT;
+  
+  if (slots.count){
+    if (slots.count.value){
+      count = parseInt(slots.count.value, 10);
+      
+      if (count < DEFAULT_HEADLINE_COUNT)
+        count = DEFAULT_HEADLINE_COUNT;
+      else if (count === 4)
+        count = 5;
+      else if (count > 5)
+        count = MAX_HEADLINE_COUNT;
+    }
+  }
+  
+  return count;
+}
+
+function getHeadlineData(threads){
+  let spokenString = '';
+  let cardString = '';
+  for (let [idx, thread] of Object.entries(threads)){
+    let ordinal = ordinalStrings[idx];
+    spokenString += ` ${ordinal} headline: ${thread.headline}.`;
+    cardString += `${parseInt(idx, 10) + 1}. ${thread.headline}\n`;
+  }
+  
+  return {
+    spokenString: spokenString,
+    cardString: cardString
+  };
+}
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetSensationalistHeadlinesHandler,
+    GetPolidicksHeadlinesHandler,
+    HelpHandler,
+    ExitHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
